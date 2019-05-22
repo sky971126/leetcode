88602463f83bc7fe32ccd42002cb1715c312bb19
sky971126 <33687022+sky971126@users.noreply.github.com>
2019-05-22 19:29:33 +0800

    Update 54.spiral-matrix.py

--- 54.spiral-matrix.py
+++ 54.spiral-matrix.py
@@ -3,24 +3,29 @@
 #
 # [54] Spiral Matrix
 #
+import math
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+    def spiralOrder(self, matrix):
+        if not matrix:
+            return []
         M = len(matrix)
         N = len(matrix[0])
         results = []
         i = 0
         j = 0
-        while i <= M//2 and i <= N//2:
-            one_row_left = (M//2 - 2 * i) == 1
-            one_column_left = (N//2 - 2 * i) == 1
-            for m in range(i, M-i):
-                results.append(matrix[i][m])
-            for n in range(i+1, M-i-1):
-                results.append(matrix[M-1-i][n])
+        while i < math.ceil(M/2) and i < math.ceil(N/2):
+            one_row_left = (M - 2 * i) == 1
+            one_column_left = (N - 2 * i) == 1
+            for n in range(i, N-i):
+                results.append(matrix[i][n])
+            for m in range(i+1, M-i-1):
+                results.append(matrix[m][N-1-i])
             if not one_row_left:
-                for m in range(M-1-i, i-1, -1):
-                    results.append(matrix[N-1-i][m])
-            if one_column_left:
-            else:
-
+                for n in range(N-1-i, i-1, -1):
+                    results.append(matrix[M-1-i][n])
+            if not one_column_left:
+                for m in range(M-2-i, i, -1):
+                    results.append(matrix[m][i])
             i += 1
+        return results
+#print(Solution().spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
